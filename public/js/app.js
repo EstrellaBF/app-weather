9141e9d95d813c03681fc471d6f80439d3a13d08
EstrellaBF <stern.von.nord@gmail.com>
2018-05-06 21:01:11 -0500

    integrating facebook API -2

--- public/js/app.js
+++ public/js/app.js
@@ -75,14 +75,14 @@ $(document).ready(function () {
     window.location.href = 'views/week.html';
   });
 
-  // facebook share button
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+  // // facebook share button
+  // (function(d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0];
+  //   if (d.getElementById(id)) return;
+  //   js = d.createElement(s); js.id = id;
+  //   js.src = 'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0';
+  //   fjs.parentNode.insertBefore(js, fjs);
+  // }(document, 'script', 'facebook-jssdk'));
 
   // Facebook code for OpenGraph
   var fbAppId = '249150542326294';
